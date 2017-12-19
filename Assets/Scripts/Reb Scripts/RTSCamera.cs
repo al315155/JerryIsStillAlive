e5b010b9e656e1b31e3dcd56ccac03358bcc5903
al315155 <al315155@uji.es>
2017-12-19 18:17:19 +0100

    nmhjgnmnm

--- Assets/Scripts/Reb Scripts/RTSCamera.cs
+++ Assets/Scripts/Reb Scripts/RTSCamera.cs
@@ -10,26 +10,37 @@ public class RTSCamera : MonoBehaviour {
 	public float velocity;
 	public float xLimit, yLimit;
 
-	void Start () {
+	bool blocked_right;
+	bool blocked_left;
+	bool blocked_up;
+	bool blocked_down;
 
+
+	void Start () {
+		blocked_right = false;
+		blocked_left = false;
+		blocked_up = false;
+		blocked_down = false;
 	}
 
 	void LateUpdate () {
 
+		CheckBlocks ();
+
 		float actualX = GetComponent<Camera> ().ScreenToViewportPoint (Input.mousePosition).x;
 		float actualY = GetComponent<Camera> ().ScreenToViewportPoint (Input.mousePosition).y;
 
-		if (actualX < xLimit && !CheckForEndOfMap ("ParedIzquierda")) {
+		if (actualX < xLimit && !blocked_left) {
 			transform.position += Vector3.left * Time.deltaTime * velocity;
 		} 
-		if (actualX > 1 - xLimit && !CheckForEndOfMap("ParedDerecha")) {
+		if (actualX > 1 - xLimit && !blocked_right) {
 			transform.position += Vector3.right * Time.deltaTime * velocity;
 		}
 
-		if (actualY < yLimit && !CheckForEndOfMap("ParedInferior")) {
+		if (actualY < yLimit && !blocked_down) {
 			transform.position += Vector3.back * Time.deltaTime * velocity;
 		}
-		if (actualY > 1 - yLimit && !CheckForEndOfMap("ParedSuperior")) {
+		if (actualY > 1 - yLimit && !blocked_up) {
 			transform.position += Vector3.forward * Time.deltaTime * velocity;
 		}
 
@@ -43,5 +54,30 @@ public class RTSCamera : MonoBehaviour {
 			}
 		}
 		return false;
+	}
+
+	public void CheckBlocks(){
+		if (CheckForEndOfMap ("ParedIzquierda"))
+			blocked_left = true;
+		else
+			blocked_left = false;
+
+		if (CheckForEndOfMap ("ParedDerecha"))
+			blocked_right = true;
+		else
+			blocked_right = false;
+
+		if (CheckForEndOfMap ("ParedInferior"))
+			blocked_down = true;
+		else
+			blocked_down = false;
+
+		if (CheckForEndOfMap ("ParedSuperior"))
+			blocked_up = true;
+		else
+			blocked_up = false;
+
+		Debug.Log (blocked_left);
+		Debug.Log (blocked_up);
 	}
 }
