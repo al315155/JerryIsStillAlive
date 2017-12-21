ab26138a567cd909a977557c9c5ae8191d07709f
alvaro <al315185@uji.es>
2017-12-21 23:36:49 +0100

    Hola Carabola

---
+++ Assets/Scripts/StateMachine/ScriptableObjects/Decisions/ReachResDecision.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "AI/ResDecision")]
+public class ReachResDecision : Decision {
+
+	public override bool Decide (StateController controller){
+
+		//Si llega al destino true y si no false;
+		return true;
+	}
+}
